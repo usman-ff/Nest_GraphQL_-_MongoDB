6b744fbd2e1dd905e36b766a3ef2089057c12a27
Usman Asif <usman.asif@futurefoundry.co>
2021-12-22 17:10:27 +0500

    Lessons Module, Integrated with GraphQL, Complated.

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,25 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateLessonInput } from './lesson.input';
+import { LessonService } from './lesson.service';
+import { LessonType } from './lesson.type';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+  @Query((returns) => LessonType)
+  lesson(@Args('id') id: string) {
+    return this.lessonService.findLessonById(id);
+  }
+
+  @Query((returns) => [LessonType]) //This is how GraphQL expects Array in return.
+  lessons() {
+    return this.lessonService.findAllLessons();
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
+  }
+}
